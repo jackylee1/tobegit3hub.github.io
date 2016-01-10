0f6c861a616b3cfba553fa2ec6d5da3a4301653e
tobe <tobeg3oogle@gmail.com>
2016-01-10 10:45:01 +0800

    Support update time in migrate script

--- migrate-old-post.py
+++ migrate-old-post.py
@@ -18,10 +18,10 @@ def main():
             new_post_name = old_post_name[11:]
             #print(new_post_name)
 
-            # Gernerate new post files
+            # 1. Gernerate new post files
             # call(["hugo", "new", "post/" + new_post_name])
 
-            # Write content to new post
+            # 2. Write content to new post
             old_post = open('/Users/tobe/code/tobegit3hub.github.io.old/_posts/' + old_post_name, 'r')
             # Example: "---header--- just content"
             post_content = old_post.read()
@@ -30,9 +30,29 @@ def main():
             old_post.close()
             
             # Append content to new file
-            new_post = open("/Users/tobe/code/tobegit3hub.github.io/content/post/" + new_post_name, "a")
-            new_post.write(post_content_no_header)
+            #new_post = open("/Users/tobe/code/tobegit3hub.github.io/content/post/" + new_post_name, "a")
+            #new_post.write(post_content_no_header)
+            #new_post.close()
+
+            # 3. Modify the time of post
+            # Example: 2012-12-31
+            year = old_post_name[0:4]
+            month = old_post_name[5:7]
+            day = old_post_name[8:10]
+            #print(year + "-" + month + "-" + day)
+
+            new_post = open("/Users/tobe/code/tobegit3hub.github.io/content/post/" + new_post_name, "r")
+            origin_post_content = new_post.read()
+
+            # Example: "2016-01-10T08:35:27+08:00" to " "2015-10-19T08:35:27+08:00"
+            new_post_content = origin_post_content.replace('-10', "-" + day, 1)
+            new_post_content = new_post_content.replace('-01', "-" + month, 1)
+            new_post_content = new_post_content.replace('2016', year, 1)
             new_post.close()
+
+            new_post_to_write = open("/Users/tobe/code/tobegit3hub.github.io/content/post/" + new_post_name, "w")
+            new_post_to_write.write(new_post_content)
+            new_post_to_write.close()
 
     print("End of migrating")
 
